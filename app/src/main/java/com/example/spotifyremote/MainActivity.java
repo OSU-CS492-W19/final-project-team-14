40569a03921ed58c7e46b65b086763505b3f0f99
Joseph Landreville <landrevillejoseph@gmail.com>
2019-02-28 20:16:47 -0800

    Implement authentication via Spotify SDK.

--- app/src/main/java/com/example/spotifyremote/MainActivity.java
+++ app/src/main/java/com/example/spotifyremote/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.spotifyremote;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final String CLIENT_ID = "bfca3780fa8947edaa1959febab111b9";
+    private static final String REDIRECT_URI = "spotifyremote://spotify/callback";
+    private static final int REQUEST_CODE = 1423;
+
+    private String mAccessToken;
+    private TextView mMainTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // grab ui
+        mMainTV = findViewById(R.id.tv_main);
+
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{"streaming"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    // Handle successful response
+                    mAccessToken = response.getAccessToken();
+                    Log.d(TAG, "successfully received access token: " + mAccessToken);
+                    connected();
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    Log.d(TAG, "failed to authenticate: " + response.getError());
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+    }
+
+    private void connected() {
+        mMainTV.setText(mAccessToken);
     }
 }
